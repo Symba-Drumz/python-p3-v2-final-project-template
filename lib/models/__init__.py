16dff6044af46905d4d41a2ffaa276839106d442
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-06-05 10:09:37 +0300

    Clean up imports for code quality

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from .models import User, Exercise, PracticeSession, Goal
+
+
 
 engine = create_engine('sqlite:///drum_practice_tracker.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-from .models import User, Exercise, PracticeSession, Goal
+
 
 __all__ = ["User", "Exercise", "PracticeSession", "Goal"]
